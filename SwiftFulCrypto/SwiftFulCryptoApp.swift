465d3b4934aaa68c47ff4241162c1417aeb5dc4b
lovNiveriya <ratedl51@gmail.com>
2024-08-25 16:18:06 +0530

    Initial Commit

---
+++ SwiftFulCrypto/SwiftFulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulCryptoApp.swift
+//  SwiftFulCrypto
+//
+//  Created by LOVE  on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftFulCryptoTests/SwiftFulCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulCryptoTests.swift
+//  SwiftFulCryptoTests
+//
+//  Created by LOVE  on 25/08/24.
+//
+
+import Testing
+@testable import SwiftFulCrypto
+
+struct SwiftFulCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
